dea2f815fc05439b5018aab5fbcd290ea243cb96
kz088103@gmail.com <kz088103@gmail.com>
2024-02-28 17:49:52 +0300

    написала исходный код

--- ConsoleApp20/oplata.cs
+++ ConsoleApp20/oplata.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace trenirovka
 {
-    internal class Bookstore
+    internal class oplata
     {
-        int i = Convert.ToInt32(Console.ReadLine());
     }
 }
